4dd36421676cb4263a370f48f30e7a53265e0281
Ahmet <75003852+adolsoft@users.noreply.github.com>
2023-12-12 22:37:23 +0100

    Açıklma satırları silindi. Her son satırda bir boşluk yapıldı

--- src/app.js
+++ src/app.js
@@ -1,29 +1,20 @@
-//console.log("Garaj uygulamamız hayırlı olsun.");
-//const express = require('express') // bu es6 gelmeden önceki versiyondur. aiğıdaki komutu kullandık.
-import express from 'express';// es6 module için bu komutu kullandık.
-import bodyParser from 'body-parser';
-//require("dotenv").config();// es6 once bu komut yazilizordu.
-import 'dotenv/config';// es6 gore 
-//const usersRoutes = require("./routes/usersRoutes");//usersRoutes.js dosyası oluşturulduktan sonra bunlar eklendi
-import usersRoutes from './routes/usersRoutes.js';//usersRoutes.js dosyası oluşturulduktan sonra bunlar eklendi
-import mongoose from 'mongoose'; // databese bağlantısı için
-
-
-
+import express from "express";
+import bodyParser from "body-parser";
+import "dotenv/config"; 
+import usersRoutes from "./routes/usersRoutes.js"; 
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// db Config Section
 const db = process.env.MongoURI;
 mongoose
   .connect(db)
   .then(console.log("Db connected"))
   .catch((err) => console.log(err));
 
-
-app.use("/users", usersRoutes);////usersRoutes.js dosyası oluşturulduktan sonra bunlar eklendi
+app.use("/users", usersRoutes);
 
 app.listen(PORT, console.log(`Server is running at PORT:${PORT}`));
--- src/models/Users.js
+++ src/models/Users.js
@@ -1,40 +1,37 @@
-//const mongoose = require('mongoose');
-import mongoose from "mongoose"; 
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    surname: {
-      type: String,
-      required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-    },
-    roles: {
-        type: Array,
-        default: ["user"],
-      },
-  });
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  roles: {
+    type: Array,
+    default: ["user"],
+  },
+});
 
-  const Users = mongoose.model("Users", userSchema);
-  
-  //module.exports = Users; // ES6 ÖNCE
-  export default Users; // ES6 SONRA // Burası user.js kullanmak için dışarıya servis etmiş olduk.
-  
+const Users = mongoose.model("Users", userSchema);
+
+export default Users; 
