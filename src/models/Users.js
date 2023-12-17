ade51f788efa028e9df0e30ad4f195d905749579
Ahmet <75003852+adolsoft@users.noreply.github.com>
2023-12-17 22:21:32 +0100

    added Users modelle isDeleteAccount property

--- src/models/Users.js
+++ src/models/Users.js
@@ -30,6 +30,10 @@ const userSchema = new mongoose.Schema({
     type: Array,
     default: ["user"],
   },
+  isDeleteAccount: {
+    type: Boolean,
+    default: false
+  },
 });
 
 const Users = mongoose.model("Users", userSchema);
