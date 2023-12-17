33717339347db046b1b8323b1ee781dc2cefdde6
Ahmet <75003852+adolsoft@users.noreply.github.com>
2023-12-17 23:09:35 +0100

    find eklendi

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {register1,register} from "../services/userServices.js";
+import { register1,register, find } from "../services/userServices.js";
 
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get("/", register1);
 
 router.post("/register", register);
+
+router.post("/:id", find);
 
 export default router;
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -2,7 +2,7 @@ import Users from "../models/Users.js";
 
 const register1 = (req, res) => {
     res.send("get isteği geldi");
-  }
+  };
 
 const register = async (req, res) => {
     const user = req.body;
@@ -12,6 +12,19 @@ const register = async (req, res) => {
     } catch (error) {
       res.status(400).json({ message: "User could not created." });
     }
-  }
+  };
 
-  export {register1, register};
+const find = async (req,res) => {
+    const id = req.params.id;
+    try {
+        const data = await Users.findOne({_id:id}).exec();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({message: "User cannot be found."});
+    }
+}
+
+
+
+
+  export {register1, register, find};
