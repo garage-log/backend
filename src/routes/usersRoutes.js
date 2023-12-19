a91ebfb22f2c00467c44ef7d04026f2401edbec1
Ahmet <75003852+adolsoft@users.noreply.github.com>
2023-12-19 19:51:12 +0100

    GL-8 bütün kullanıcıları getirme

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { getIstegi, login, register, find } from "../services/usersServices.js";
+import { getIstegi, login, register, find, allUsers } from "../services/usersServices.js";
 
 const router = express.Router();
 
-router.get("/", getIstegi);
+//router.get("/", getIstegi);
 router.post("/login", login);
 router.post("/register", register);
-router.post("/register", register);
-router.post("/:id", find);
+router.get("/:id", find);
+router.post("/hepsi", allUsers);
+
+//burada get isteği  ( / ) iken çalısıyor. Ama /hepsi durumda ike .alışmıyor bunu nedeni nedir.
+//router.get("/hepsi", allUsers);
 
 export default router;
--- src/services/usersServices.js
+++ src/services/usersServices.js
@@ -9,7 +9,13 @@ const login = async (req, res) => {
   const data = await Users.findOne({ username }).exec();
   if (!data) {
     return res.status(404).json({message: "User cannot be found"});
+  };
+  
+  console.log(data.password) /// Burayı hashe ile kontrol edilecek.
+  if(!(data.password === password)) {
+    return res.status(403).json({ message: " The password is incorrect"})
   }
+  
   res.status(200).json(data);
 }
 
@@ -23,14 +29,41 @@ const register = async (req, res) => {
     }
   };
 
-const find = async (req,res) => {
-    const id = req.params.id;
-    try {
-        const data = await Users.findOne({_id:id}).exec();
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({message: "User cannot be found."});
-    }
-}
+const find = async(req, res) =>{
+  const id = req.params.id;
+  try {
+      const data = await Users.findOne({_id: id}).exec();
+      
+      res.json({
+          user: {
+              id: data._id,
+              username:data.username,
+              email:data.email,
+          }
+      })
 
-  export {getIstegi, login, register, find};
+  } catch (error) {
+      res.status(403).json({message:" User could not found."});
+  }
+};
+
+const allUsers = async(req, res) =>{
+    
+  try {
+      const data = await Users.find().exec();
+      const user = data.map((item) => {
+          return {
+              id: item._id,
+              username:item.username,
+              email:item.email,
+          }
+      })
+
+      res.json({user});
+
+  } catch (error) {
+      res.status(403).json({message:" User could not found."});
+  }
+};
+
+  export {getIstegi, login, register, find, allUsers};
