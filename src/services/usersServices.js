4ff1f87bc7b6dbd32e211f1afd38c1fdb26c3039
Ahmet <75003852+adolsoft@users.noreply.github.com>
2023-12-24 13:57:29 +0100

    GL-12 login işlemi yaparken token oluşturuyor.

--- src/services/usersServices.js
+++ src/services/usersServices.js
@@ -1,94 +1,100 @@
 import Users from "../models/Users.js";
+import jwt from "jsonwebtoken";
 
 const login = async (req, res) => {
   const { username, password } = req.body;
   const data = await Users.findOne({ username }).exec();
   if (!data) {
-    return res.status(404).json({message: "User cannot be found"}); 
+    return res.status(404).json({ message: "User cannot be found" });
+  }
+
+  const isValidated = await data.validatePassword(password);
+  if (!isValidated) {
+    return res.status(403).json({ message: " The password is incorrect" });
+  }
+
+  const user = {
+    id: data._id,
+    username: data.username,
+    email: data.email,
   };
   
-  const isValidated = await data.validatePassword(password);
-  if(!isValidated) {
-    return res.status(403).json({ message: " The password is incorrect"})
-  }
-  
-  res.status(200).json(data);
-}
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN); // yukarıdaki user objesini şifreler. ve değişken atar.
+  res.json({
+    user: {
+      token: accessToken,
+      ...user, // user tokeni ekleyip gonderiri
+    },
+  });
+};
 
 const register = async (req, res) => {
-    const user = req.body;
-    try {
-      const data = await Users.create(user);
-      res.status(200).json(data);
-    } catch (error) {
-      res.status(400).json({ message: "User could not created." });
-    }
-  };
-
-const find = async(req, res) =>{
-  const id = req.params.id;
-  try {
-      const data = await Users.findOne({_id: id}).exec();
-      
-      res.json({
-          user: {
-              id: data._id,
-              username:data.username,
-              email:data.email,
-              password:data.password,
-          }
-      })
-
-  } catch (error) {
-      res.status(403).json({message:" User could not found."});
-  }
-};
-
-const allUsers = async(req, res) =>{
-    
-  try {
-      const data = await Users.find().exec();
-      const user = data.map((item) => {
-          return {
-              id: item._id,
-              username:item.username,
-              email:item.email,
-              password:item.password,
-          }
-      }
-      )
-
-      res.json({user});
-
-  } catch (error) {
-      res.status(403).json({message:" User could not found."});
-  }
-};
-
-const update = async(req, res) => {
-  
-  console.log(req.body)
   const user = req.body;
   try {
-      const data = await Users.updateOne({_id: user.id}, {$set: user}).exec();
-      
-      res.json(data);
-
+    const data = await Users.create(user);
+    res.status(200).json(data);
   } catch (error) {
-      res.status(403).json({message:" User could not update."});
-  } 
+    res.status(400).json({ message: "User could not created." });
+  }
 };
 
-const remove = async(req, res) => {
+const find = async (req, res) => {
   const id = req.params.id;
   try {
-      const data = await Users.deleteOne({_id: id}).exec();
-      
-      res.json(data);
+    const data = await Users.findOne({ _id: id }).exec();
 
+    res.json({
+      user: {
+        id: data._id,
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      },
+    });
   } catch (error) {
-      res.status(403).json({message:" User could not delete."});
-  } 
+    res.status(403).json({ message: " User could not found." });
+  }
 };
 
-export {login, register, find, allUsers, update, remove};
+const allUsers = async (req, res) => {
+  try {
+    const data = await Users.find().exec();
+    const user = data.map((item) => {
+      return {
+        id: item._id,
+        username: item.username,
+        email: item.email,
+        password: item.password,
+      };
+    });
+
+    res.json({ user });
+  } catch (error) {
+    res.status(403).json({ message: " User could not found." });
+  }
+};
+
+const update = async (req, res) => {
+  console.log(req.body);
+  const user = req.body;
+  try {
+    const data = await Users.updateOne({ _id: user.id }, { $set: user }).exec();
+
+    res.json(data);
+  } catch (error) {
+    res.status(403).json({ message: " User could not update." });
+  }
+};
+
+const remove = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const data = await Users.deleteOne({ _id: id }).exec();
+
+    res.json(data);
+  } catch (error) {
+    res.status(403).json({ message: " User could not delete." });
+  }
+};
+
+export { login, register, find, allUsers, update, remove };
